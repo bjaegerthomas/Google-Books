e2f3ac0051b0b7ead80e5ec25f95f6d931d16154
jaegerthomas <bjaegerthomas@gmail.com>
2025-03-19 14:28:19 -0400

    updated server.ts for type defs, resolvers, and apollo

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,9 +2,11 @@ import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import path from 'node:path';
-
+import type { Request, Response } from 'express';
 import { typeDefs, resolvers } from './schemas/index.js';
+
 import db from './config/connection.js';
+import { authenticateToken } from './services/auth.js';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,17 +17,21 @@ const server = new ApolloServer({
 
 const startApolloServer = async () => {
   await server.start();
+  await db;
 
-  app.use(express.urlencoded({ extended: true }));
+  app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
 
-  app.use('/graphql', expressMiddleware(server));
+  app.use('/graphql', expressMiddleware(server as any,
+    {
+      context: authenticateToken as any
+    }));
 
   // if we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
-    app.get('*', (_req, res) => {
+    app.get('*', (_req: Request, res: Response) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
   }
