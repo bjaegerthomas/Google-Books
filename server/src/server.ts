2266613fffabc75b07284e66e0c4d17f02ec4991
jaegerthomas <bjaegerthomas@gmail.com>
2025-04-02 19:36:10 -0400

    added schemas index, update to connection.ts and server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,14 +2,12 @@ import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import path from 'node:path';
-import type { Request, Response } from 'express';
+
 import { typeDefs, resolvers } from './schemas/index.js';
-
 import db from './config/connection.js';
-import { authenticateToken } from './services/auth.js';
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -17,26 +15,20 @@ const server = new ApolloServer({
 
 const startApolloServer = async () => {
   await server.start();
-  await db;
+  await db();
 
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
 
-  app.use('/graphql', expressMiddleware(server as any,
-    {
-      context: authenticateToken as any
-    }));
-
-  // if we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
-    app.get('*', (_req: Request, res: Response) => {
+    app.get('*', (_req, res) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
   }
 
-  db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+  app.use('/graphql', expressMiddleware(server));
 
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
