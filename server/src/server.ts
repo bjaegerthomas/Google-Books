72aeeeb423145a0827c89d33bde187db7918af6b
jaegerthomas <bjaegerthomas@gmail.com>
2025-04-22 12:06:11 -0400

    updating server.ts for graphql and authentication

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import path from 'node:path';
+import { authenticateToken } from './services/auth-service.js';
 
 import { typeDefs, resolvers } from './schemas/index.js';
 import db from './config/connection.js';
@@ -20,6 +21,12 @@ const startApolloServer = async () => {
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
 
+  app.use('/graphql', expressMiddleware(server as any,
+      {
+        context: authenticateToken as any
+      }
+    ));
+
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
@@ -27,8 +34,6 @@ const startApolloServer = async () => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
   }
-
-  app.use('/graphql', expressMiddleware(server));
 
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
